3ac72dc158000c5b494d705fae34497af347fbf9
Faisal Akhtar <aimsbrains073@gmail.com>
2025-01-16 08:48:10 -0600

    first commit

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="main-footer">
+    <strong>{{ __('common.Copyright') }} &copy; {{date('Y')}} <a href="http://github.com/faisal336">Faisal</a>.</strong> {{ __('common.all_reserved') }}
+</footer>
